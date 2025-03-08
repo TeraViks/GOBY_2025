106ea5e8a012da77f9bc0da92432e948a95eb822
Nathan Pulsipher <npulsipher4@gmail.com>
2025-03-08 13:03:21 -0800

    Update TeleopDrive PIDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -44,7 +44,7 @@ public final class Constants {
 
   // Tunable constants are disabled unless this is set to true.
   // Intended to remain false in committed code.
-  public static final boolean kEnableTuning = true;
+  public static final boolean kEnableTuning = false;
 
   public static final class LightConstants {
     public static final int kBlinkinPWMInput = 0;
@@ -147,7 +147,7 @@ public final class Constants {
       kAutoPIDFSlotID
     );
     public static final SparkUtil.PIDFSlot kTeleopDrivePIDFSlot = new SparkUtil.PIDFSlot(
-      new PIDF(0.375, 0.0, 0.0, 0.25),
+      new PIDF(0.3, 0.0, 0.0, 0.25),
       kTeleopPIDFSlotID
     );
     public static final SparkUtil.Config kFrontLeftDriveMotorConfig = new SparkUtil.Config(

934e22a501e12d96f7554eb4e676f3a16a6acf98
keira1008 <>
2025-03-29 10:13:33 -0700

    increase kPositionLoAlgae height by 2cm

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -611,7 +611,7 @@ public final class Constants {
     public static final Translation2d kPositionL3 =
       new Translation2d(Units.degreesToRadians(-35.0), 1.62);
     public static final Translation2d kPositionLoAlgae =
-      new Translation2d(Units.degreesToRadians(-20.0), 0.85);
+      new Translation2d(Units.degreesToRadians(-20.0), 0.87);
     public static final Translation2d kPositionHiAlgae =
       new Translation2d(Units.degreesToRadians(-20.0), 1.22);
     public static final Translation2d kPositionIntake =
